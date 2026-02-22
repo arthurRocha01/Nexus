6b8f7b30cde3237c99f59790d289686ed100472f
Arthur Rocha <arthurrocha007.net@gmail.com>
2026-02-22 11:48:35 -0300

    feat: adiciona service

--- src/main/java/com/arthurrocha/nexus/application/ProductService.java
+++ src/main/java/com/arthurrocha/nexus/application/ProductService.java
@@ -1,5 +1,21 @@
 package com.arthurrocha.nexus.application;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.arthurrocha.nexus.domain.Product;
+import com.arthurrocha.nexus.infrastructure.client.GdoorProductClient;
+
+@Service
 public class ProductService {
-  
+  private final GdoorProductClient gdoorClient; 
+
+  public ProductService(GdoorProductClient gdoorClient) {
+    this.gdoorClient = gdoorClient;
+  }
+
+  public List<Product> listProducts() {
+    return this.gdoorClient.fetchAll();
+  }
 }
