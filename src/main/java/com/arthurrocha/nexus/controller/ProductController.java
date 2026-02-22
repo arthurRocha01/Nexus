37edeed52a37a637596d731de799d39ceb0b40bb
Arthur Rocha <arthurrocha007.net@gmail.com>
2026-02-22 11:53:53 -0300

    feat: adiciona controller

--- src/main/java/com/arthurrocha/nexus/controller/ProductController.java
+++ src/main/java/com/arthurrocha/nexus/controller/ProductController.java
@@ -1,5 +1,27 @@
 package com.arthurrocha.nexus.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.arthurrocha.nexus.application.ProductService;
+import com.arthurrocha.nexus.domain.Product;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
-  
+  private final ProductService service;
+
+  public ProductController(ProductService service) {
+    this.service = service;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Product>> list() {
+    List<Product> products = this.service.listProducts();
+    return ResponseEntity.ok(products);
+  }
 }
