f56cae4f927d475e44f68363bebd3f7d6d90d55d
Arthur Rocha <arthurrocha007.net@gmail.com>
2026-02-21 21:06:13 -0300

    feat: adiciona Product entity

--- src/main/java/com/arthurrocha/nexus/domain/Product.java
+++ src/main/java/com/arthurrocha/nexus/domain/Product.java
@@ -1,5 +1,43 @@
 package com.arthurrocha.nexus.domain;
 
+import java.math.BigDecimal;
+
 public class Product {
-  
+  private String id;
+  private String description;
+  private int quantity;
+  private BigDecimal price;
+  private String ncm;
+
+  private Product(String id, String description, int quantity, BigDecimal price, String ncm) {
+    this.id = id;
+    this.description = description;
+    this.quantity = quantity;
+    this.price = price;
+    this.ncm = ncm;
+  }
+
+  public static Product create(String id, String description, int quantity, BigDecimal price, String ncm) {
+    return new Product(id, description, quantity, price, ncm);
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getDescription() {
+    return description;
+    }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public String getNcm() {
+    return ncm;
+  }
 }
